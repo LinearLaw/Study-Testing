878dc0fac39647a667f2275b78878013930d6c5e
LinearLaw <17817363770@163.com>
2020-07-26 16:06:05 +0800

    remame

---
+++ java/code/Test_20_1_ListAllFile.java
@@ -0,0 +1,18 @@
+
+/**
+ * @desc 列出某一个文件夹下面的所有文件和文件夹，包括子目录
+ * 
+ * 递归
+ * 1、限定条件
+ * 2、递归次数，避免内存溢出
+ */
+
+import java.io.*;
+public class ListAllFile {
+
+    public static void ShowDir(File dir,int level){
+        
+    }
+
+
+}
