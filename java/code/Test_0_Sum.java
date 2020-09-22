de07270dde53538d7ec28269140cb3a66f517015
LinearLaw <17817363770@163.com>
2020-09-22 22:41:20 +0800

    update test

--- java/code/Test_0_Sum.java
+++ java/code/Test_0_Sum.java
@@ -4,21 +4,25 @@ import java.lang.String;
 
 class Solution{
     public static void main(String[] args){
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        if(false){
+            Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+    
+            map.put(3,4);
+            map.put(4,5);
+            map.put(6,7);
+            map.put(7,8);
+    
+            int c;
+            try{
+                c = map.get(10);
+                if(String.valueOf(c).equals("")){
+                    System.out.println(c);
+                }
+            }catch(Exception e){}
+        }
 
-        map.put(3,4);
-        map.put(4,5);
-        map.put(6,7);
-        map.put(7,8);
-
-        int c;
-        try{
-            c = map.get(10);
-            if(String.valueOf(c).equals("")){
-                System.out.println(c);
-            }
-        }catch(Exception e){
-
+        if(true){
+            System.out.println(100000000 + "::::::::" + 900000000*10);
         }
 
     }
