67482ebbbf30ca14d1ffb9ee2df8cd9a78bee6d0
LinearLaw <17817363770@163.com>
2021-02-07 23:34:13 +0800

    【2021-2-7 23:33:57】Add string test

---
+++ c++/cpp/Base_1/Basic_1/Basic05_String/Basic05_String.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TestString {
+
+public:
+	void reverseString() {
+		string a = "-2313";
+		string b = "1516418";
+
+		// string拼接；
+		string c = b[0] + a;
+
+		cout << c << endl;
+	}
+
+};
+
+int main()
+{
+	TestString().reverseString();
+}
