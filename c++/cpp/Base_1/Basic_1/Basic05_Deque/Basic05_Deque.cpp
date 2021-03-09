c490232ed051f04eb6ef7b9f8b1f1613cf03cc8f
LinearLaw <17817363770@163.com>
2021-03-09 23:36:16 +0800

    【2021-3-9 23:35:35】

---
+++ c++/cpp/Base_1/Basic_1/Basic05_Deque/Basic05_Deque.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+class TestDeque {
+
+public:
+	void testPopFront() {
+		/*
+			获取第一个元素：q.front()  -> Not q.begin()
+			获取末尾元素：q.back()  -> Not q.end()
+
+			q.pop_back()
+			q.pop_front()
+			q.push_back()
+			q.push_front()
+		*/
+	}
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
