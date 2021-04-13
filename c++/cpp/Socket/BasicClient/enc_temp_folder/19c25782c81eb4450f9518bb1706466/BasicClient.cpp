092e4963adb36e2db9b4c5b482b4ac35626d9163
LinearLaw <17817363770@163.com>
2021-04-13 22:24:19 +0800

    【2021-4-13 22:24:01】Add socket relate reference

--- c++/cpp/Socket/BasicClient/enc_temp_folder/19c25782c81eb4450f9518bb1706466/BasicClient.cpp
+++ c++/cpp/Socket/BasicClient/enc_temp_folder/19c25782c81eb4450f9518bb1706466/BasicClient.cpp
@@ -7,33 +7,21 @@ using namespace std;
 void initialization();
 
 int main()
-{	
-	/*  发送的字符串长度，
-		接收的字符串长度 
-	*/
+{
 	int send_len = 0;
 	int recv_len = 0;
 
-	/*	发送的字符串缓冲
-		接收的字符串缓冲
-	*/
 	char send_buf[100];
 	char recv_buf[100];
 
-	/*	定义服务端socket
-		定义服务端地址	
-	*/
 	SOCKET s_server;
 	SOCKADDR_IN server_addr;
 	initialization();
 
-	/*	设置服务端的地址信息
-	*/
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
 	server_addr.sin_port = htons(5010);
 
-	/*  创建socket并connect到服务端 */
 	s_server = socket(AF_INET, SOCK_STREAM, 0);
 	if(connect(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR))
 		== SOCKET_ERROR	
@@ -47,7 +35,6 @@ int main()
 	}
 
 	while (1) {
-		/*  输入内容，作为client发送的内容，将其send到服务端 */
 		cout << "[Client]：";
 		cin >> send_buf;
 		send_len = send(s_server, send_buf, 100, 0);
@@ -56,8 +43,6 @@ int main()
 			cout << "[Fail] 发送失败" << endl;
 			break;
 		}
-
-		/*  等待server的返回结果  */
 		recv_len = recv(s_server, recv_buf, 100, 0);
 		if (recv_len < 0) {
 			cout << "[Fail] 接收失败" << endl;
@@ -73,7 +58,6 @@ int main()
 	return 0;
 }
 
-/*	对socket库进行初始化 */
 void initialization() {
 	WORD w_req = MAKEWORD(2, 2);
 	WSADATA wsadata;
--- c++/cpp/Socket/BasicServer/BasicServer/BasicServer.cpp
+++ c++/cpp/Socket/BasicServer/BasicServer/BasicServer.cpp
@@ -7,29 +7,44 @@ using namespace std;
 
 void initialization();
 
+struct S{};
+
 
 int main()
-{
+{	
+	cout << "SIZE:" << sizeof(S) << endl;
+	cout << endl;
+
+	/* 	发送数据的长度	
+		接收数据的长度
+		SOCKADDR的大小
+	 */
 	int send_len = 0;
 	int recv_len = 0;
 	int len = 0;
 
+	/*	接收和发送数据的字符缓冲区 */
 	char send_buf[100];
 	char recv_buf[100];
 
+	/*	定义服务端的Socket和接受请求的Socket*/
 	SOCKET s_server;
 	SOCKET s_accept;
 
+	/*	服务端地址，接收请求地址*/
 	SOCKADDR_IN server_addr;
 	SOCKADDR_IN accept_addr;
 	initialization();
 
+	/*
+		服务端信息
+	 */
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(5010);
 
+	/*	创建socket并bind */
 	s_server = socket(AF_INET, SOCK_STREAM, 0);
-
 	if (bind(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR))
 		== SOCKET_ERROR	
 	) {
@@ -39,6 +54,7 @@ int main()
 		cout << "[Success] Socket bind success" << endl;
 	}
 
+	/*	设置socket监听 */
 	if (listen(s_server, SOMAXCONN) < 0) {
 		cout << "[Fail] 设置监听状态失败" << endl;
 		WSACleanup();
@@ -49,6 +65,7 @@ int main()
 	cout << "服务器正在监听连接，请稍后..." << endl;
 	cout << endl;
 
+	/*	接收连接请求  */
 	len = sizeof(SOCKADDR);
 	s_accept = accept(s_server, (SOCKADDR *)&accept_addr, &len);
 	if (s_accept == SOCKET_ERROR) {
@@ -59,6 +76,7 @@ int main()
 
 	cout << "连接已建立，准备接受数据" << endl;
 	while (1) {
+		/*	等待接收由Client发送来的数据，  */
 		recv_len = recv(s_accept, recv_buf, 100, 0);
 		if(recv_len < 0){
 			cout << "[Fail] 接收失败" << endl;
@@ -68,6 +86,7 @@ int main()
 			cout << "[Client]：" << recv_buf << endl;
 		}
 
+		/*	输入要发送的内容，返回结果 */
 		cout << "[Server]：";
 		cin >> send_buf;
 		send_len = send(s_accept, send_buf, 100, 0);
@@ -77,13 +96,17 @@ int main()
 		}
 	}
 
+	/*  关闭socket  */
 	closesocket(s_server);
 	closesocket(s_accept);
-
+	/*  释放DLL资源  */
 	WSACleanup();
 	return 0;
 }
 
+/*
+	初始化 Socket 库
+*/
 void initialization() {
 	WORD w_req = MAKEWORD(2, 2);
 	WSADATA wsadata;
