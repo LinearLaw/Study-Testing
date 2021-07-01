54e0d25d4fb06a7c1ceec05eb5320b49fd11242b
LinearLaw <17817363770@163.com>
2021-07-01 23:02:41 +0800

    【2021-07-1 22:54:38】 update springboot + mybatis

--- java/code/w89_SpringBoot_Mybatis/src/main/java/com/example/w89_springboot_mybatis/W89SpringbootMybatisApplication.java
+++ java/code/w89_SpringBoot_Mybatis/src/main/java/com/example/w89_springboot_mybatis/W89SpringbootMybatisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class W89SpringbootMybatisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(W89SpringbootMybatisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(W89SpringbootMybatisApplication.class, args);
+    }
 
 }
--- java/code/w89_SpringBoot_Mybatis/src/test/java/com/example/w89_springboot_mybatis/W89SpringbootMybatisApplicationTests.java
+++ java/code/w89_SpringBoot_Mybatis/src/test/java/com/example/w89_springboot_mybatis/W89SpringbootMybatisApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class W89SpringbootMybatisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
